ef775b395a5808060cbae8a2a19705a3212068dd
klaus-gu <40054366+klaus-gu@users.noreply.github.com>
2023-03-22 09:40:49 +0800

    OpenAISettingFactory 支持通过Spring Bean 的方式配置

--- chatgpt-client/src/main/java/xyz/openai/chatgpt/client/util/OpenAISettingCheckUtil.java
+++ chatgpt-client/src/main/java/xyz/openai/chatgpt/client/util/OpenAISettingCheckUtil.java
@@ -10,31 +10,35 @@ import xyz.openai.chatgpt.client.setting.OpenAISetting;
  * @program chatgpt
  **/
 public class OpenAISettingCheckUtil {
-    public static void check(OpenAISetting openAISetting){
-        if (ObjectUtils.allNull(openAISetting.enableApiKeyAuthorization,openAISetting.enableTokenAuthorization)){
-            throw new IllegalArgumentException("【OpenAISetting init error】at least one authorization type should be setted");
+    
+    public static void check(OpenAISetting openAISetting) {
+        if (ObjectUtils.allNull(openAISetting.enableApiKeyAuthorization, openAISetting.enableTokenAuthorization)) {
+            throw new IllegalArgumentException(
+                    "【OpenAISetting init error】at least one authorization type should be setted");
         }
-        if (openAISetting.enableTokenAuthorization != null && openAISetting.enableTokenAuthorization){
-            throw  new IllegalArgumentException("【OpenAISetting init error】token authorization is not support");
-//            if(StringUtils.isAllEmpty(openAISetting.password,openAISetting.email)){
-//                throw new IllegalArgumentException("【OpenAISetting init error】email and password should not be empty when token authorization type is enabled");
-//            }
-        }
-        
-        if (openAISetting.enableApiKeyAuthorization != null && !openAISetting.enableApiKeyAuthorization){
-            throw  new IllegalArgumentException("【OpenAISetting init error】api-key authorization support only");
+        if (openAISetting.enableTokenAuthorization != null && openAISetting.enableTokenAuthorization) {
+            throw new IllegalArgumentException("【OpenAISetting init error】token authorization is not support");
+            //            if(StringUtils.isAllEmpty(openAISetting.password,openAISetting.email)){
+            //                throw new IllegalArgumentException("【OpenAISetting init error】email and password should not be empty when token authorization type is enabled");
+            //            }
         }
         
-        if (openAISetting.enableApiKeyAuthorization && StringUtils.isEmpty(openAISetting.apiKey)){
-            throw new IllegalArgumentException("【OpenAISetting init error】api-key should not be empty when api-key authorization type is enabled");
+        if (openAISetting.enableApiKeyAuthorization != null && !openAISetting.enableApiKeyAuthorization) {
+            throw new IllegalArgumentException("【OpenAISetting init error】api-key authorization support only");
         }
         
-        if (openAISetting.modelToUse ==null){
+        if (openAISetting.enableApiKeyAuthorization && StringUtils.isEmpty(openAISetting.apiKey)) {
+            throw new IllegalArgumentException(
+                    "【OpenAISetting init error】api-key should not be empty when api-key authorization type is enabled");
+        }
+        
+        if (openAISetting.modelToUse == null) {
             throw new IllegalArgumentException("【OpenAISetting init error】at least one model type should be setted");
         }
         
-        if (openAISetting.enableProxy && StringUtils.isAnyEmpty(openAISetting.proxyPort,openAISetting.proxyHostname)){
-            throw new IllegalArgumentException("【OpenAISetting init error】proxy port and hostname should set when proxy is enabled");
+        if (openAISetting.enableProxy && StringUtils.isAnyEmpty(openAISetting.proxyPort, openAISetting.proxyHostname)) {
+            throw new IllegalArgumentException(
+                    "【OpenAISetting init error】proxy port and hostname should not be null when proxy is enabled");
         }
     }
 }
