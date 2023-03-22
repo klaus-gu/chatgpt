3df4176080f8db038117b48b3d9a7974f4f102a3
klaus-gu <40054366+klaus-gu@users.noreply.github.com>
2023-03-22 09:42:30 +0800

    example

--- chatgpt-client-example/src/main/java/xyz/openai/chatgpt/client/example/XOpenAISettingFactory.java
+++ chatgpt-client-example/src/main/java/xyz/openai/chatgpt/client/example/XOpenAISettingFactory.java
@@ -1,5 +1,6 @@
 package xyz.openai.chatgpt.client.example;
 
+import org.springframework.stereotype.Component;
 import xyz.openai.chatgpt.client.setting.OpenAISetting;
 import xyz.openai.chatgpt.client.spring.core.factory.OpenAISettingFactory;
 
@@ -8,13 +9,13 @@ import xyz.openai.chatgpt.client.spring.core.factory.OpenAISettingFactory;
  * @author <a href="mailto:guyue375@outlook.com">Klaus.turbo</a>
  * @program chatgpt
  **/
+@Component
 public class XOpenAISettingFactory implements OpenAISettingFactory {
     
     @Override
     public OpenAISetting getSetting() {
         final OpenAISetting openAISetting = new OpenAISetting();
         openAISetting.apiKey = "******";
-        openAISetting.enableRequestDebug = true;
         return openAISetting;
     }
 }
